249b1802b87d067fa59d11039bfc242e72ead78d
Jason Jarrett <jason@elegantcode.com>
2014-04-21 10:17:36 -0700

    file-maning output

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ var gulp = require('gulp');
 
 var bulp = require('./lib/bulp');
 
+var _config = {
+    blogSlugFormat: "/:year/:month/:title/"
+}
+
+
 var bulpPlugin = (function() {
 
     // through2 is a thin wrapper around node transform streams
@@ -27,7 +32,6 @@ var bulpPlugin = (function() {
 
             var metadata;
 
-
             if (file.isBuffer()) {
                 metadata = bulp.getPage(file.contents.toString('utf-8'));
 
@@ -37,6 +41,31 @@ var bulpPlugin = (function() {
             if (file.isStream()) {
                 throw new PluginError(PLUGIN_NAME, "Doesn't support streams (yet?)!");
             }
+
+            var path = require('path');
+            var dirName = path.dirname(file.path);
+
+            function pad(n){return n<10 ? '0'+n : n}
+
+            var date = metadata.options.date;
+
+            var baseName = path.basename(file.path);
+            var blogFolderName = baseName.substr(0, baseName.length - path.extname(baseName).length).substr(11);
+
+            var slugTokenYear = "" + date.getUTCFullYear();
+            var slugTokenMonth = "" + pad(date.getUTCMonth());
+            var slugTokenDay = "" + pad(date.getUTCDay());
+            var slugTokenTitle = "" + blogFolderName;
+
+            var newFileName = _config.blogSlugFormat
+                .replace(":year", slugTokenYear)
+                .replace(":month", slugTokenMonth)
+                .replace(":day", slugTokenDay)
+                .replace(":title", slugTokenTitle);
+
+            newFileName = path.join(newFileName, "index.html");
+            var newFilePath = path.normalize(dirName + "/" + newFileName);
+            file.path = newFilePath;
 
             pages[file.path] = metadata;
 
@@ -68,7 +97,7 @@ gulp.task('default', function () {
         .pipe(debug({verbose: true}))
         .pipe(liquid({
             locals: {
-                name: "Jason"
+                name: "Dave"
             }
         }))
         .pipe(markdown())
