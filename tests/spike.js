811fc6ec5b0f1c73d6c92749e9b4b461401fa3f8
Jason Jarrett <jason@elegantcode.com>
2014-04-21 10:18:41 -0700

    fix test

--- tests/spike.js
+++ tests/spike.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 
 describe('Parsing a page', function () {
     it('Page gets parsed', function () {
-        var filePath = 'tests/samplePosts/2014-20-01.md';
+        var filePath = 'tests/samplePosts/2014-20-01-this-is-my-title.md';
 
         var contents = fs.readFileSync(filePath, 'utf-8');
 
