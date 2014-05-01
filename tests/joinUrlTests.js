f176cb8bbe1db2aa10dd92f9a0f86ba9efc38d9b
Jason Jarrett <jason@elegantcode.com>
2014-04-30 22:09:23 -0700

    lots of cleanup

---
+++ tests/joinUrlTests.js
@@ -0,0 +1,78 @@
+require('approvals').configure({
+    reporters: ["visualstudio", "opendiff"]
+}).mocha(__dirname);
+
+var joinUrl = require("../lib/joinUrl");
+
+it('can join url\'s ', function () {
+
+    var urls = [
+        {
+            comment: "basic join",
+            expected: "basic/join",
+            actual: null,
+            args: ["basic", "join"],
+        },
+        {
+            comment: "null first",
+            expected: "join",
+            actual: null,
+            args: [null, "join"],
+        },
+        {
+            comment: "null second",
+            expected: "basic",
+            actual: null,
+            args: ["basic", null],
+        },
+        {
+            comment: "null middle",
+            expected: "basic/join",
+            actual: null,
+            args: ["basic", null, "join"],
+        },
+        {
+            comment: "starts with a slash",
+            expected: "/basic/join",
+            actual: null,
+            args: ["/basic", "join"],
+        },
+        {
+            comment: "already has slash",
+            expected: "basic/join",
+            actual: null,
+            args: ["basic", "/join"],
+        },
+        {
+            comment: "slash at end",
+            expected: "basic/join/",
+            actual: null,
+            args: ["basic", "join/"],
+        },
+        {
+            comment: "wrong slash direction to start",
+            expected: "/basic/join/",
+            actual: null,
+            args: ["\\basic\\", "\\join\\"],
+        },
+        {
+            comment: "argument is only a slash",
+            expected: "/join",
+            actual: null,
+            args: ["/", "join"],
+        },
+        
+    ]
+
+    var result = urls.map(function (item) {
+        item.actual = joinUrl.apply(null, item.args);
+
+        if (item.expected !== item.actual) {
+            item.ERROR = "!!!!!!!!!!!!!! NOPE !!!!!!!!!!!!!!!!!!!";
+        }
+
+        return item;
+    });
+        
+    this.verifyAsJSON(result);
+});
