c3a7f00eabf815a4413da8e9bfb5b99d6ed79caa
Jason Jarrett <jason@elegantcode.com>
2015-01-03 10:38:46 -0800

    adding some tests

---
+++ test/plugins/gistTests.js
@@ -0,0 +1,54 @@
+
+/*jshint expr: true*/
+'use strict';
+require('approvals').configure({
+  reporters: ["visualstudio", "opendiff"]
+}).mocha(__dirname);
+
+//var expect = require('chai').expect;
+var assert = require('stream-assert');
+var gutil = require('gulp-util');
+var File = gutil.File;
+
+var sut = require("../../lib/plugins/gist");
+
+describe('alias', function () {
+
+  it('Should token replace gists with a file', function (done) {
+    var stream = sut();
+
+    var that = this;
+    stream
+    .pipe(assert.length(1))
+    .pipe(assert.nth(0, function (file) {
+      that.verify(file.contents.toString());
+    }))
+    .pipe(assert.end(done));
+
+    var file = new File();
+    file.contents = new Buffer("this is some test content with a gist: {{ gist FFFFFFFFFF \"testFile.js\" }}");
+
+    stream.write(file);
+    stream.end();
+  });
+
+
+  it('Should token replace gists with bracket', function (done) {
+    var stream = sut();
+
+    var that = this;
+    stream
+    .pipe(assert.length(1))
+    .pipe(assert.nth(0, function (file) {
+      that.verify(file.contents.toString());
+    }))
+    .pipe(assert.end(done));
+
+    var file = new File();
+    file.contents = new Buffer("this is some test content with a gist: [gist id=FFFFFFFFFF]");
+
+    stream.write(file);
+    stream.end();
+  });
+
+});
